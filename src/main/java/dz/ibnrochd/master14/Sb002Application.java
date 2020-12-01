5ceee37f663a7b644a1c345e34eab30a4ae601fa
Amine MERICHA <aminemer@yahoo.fr>
2020-12-01 14:23:04 +0100

    TP-11

--- src/main/java/dz/ibnrochd/master14/Sb002Application.java
+++ src/main/java/dz/ibnrochd/master14/Sb002Application.java
@@ -17,7 +17,10 @@ import dz.ibnrochd.master14.dao.TraitementRepository;
 import dz.ibnrochd.master14.model.Patient;
 
 @SpringBootApplication
+
 public class Sb002Application implements CommandLineRunner {
+	
+	
 	
 	@Autowired
 	PatientRepository patientRepository;
@@ -38,32 +41,32 @@ public class Sb002Application implements CommandLineRunner {
 	
 
 	public static void main(String[] args) {
-		SpringApplication.run(Sb002Application.class, args);
+	SpringApplication.run(Sb002Application.class, args);
 	}
 
 	@Override
 	public void run(String... args) throws Exception {
 		
-		System.out.println("---------------------------récupérer la liste de tous les patients puis afficher leurs noms------------------------------------");
+		//System.out.println("---------------------------récupérer la liste de tous les patients puis afficher leurs noms------------------------------------");
 		// TODO : récupérer la liste de tous les patients puis afficher leurs noms
-		List<Patient> L2=patientRepository.findAll();
-		for(Patient l2:L2)
-		System.out.println(l2.getNom()+"  "+l2.getPrenom());
+		//List<Patient> L2=patientRepository.findAll();
+		//for(Patient l2:L2)
+		//System.out.println(l2.getNom()+"  "+l2.getPrenom());
 		
 		
 		
-		System.out.println("---------------------------rechercher les patients ayant le nom \"Yahi\" puis leurs prénoms------------------------------------");	
+		//System.out.println("---------------------------rechercher les patients ayant le nom \"Yahi\" puis leurs prénoms------------------------------------");	
 		// TODO : rechercher les patients ayant le nom "Yahi" puis leurs prénoms
 		
 		
-		List<Patient> L3 = patientRepository.findBynom("Yahi");
+		//List<Patient> L3 = patientRepository.findBynom("Yahi");
 		
-		for(Patient l3:L3)
-				System.out.println(l3.getId()+"  "+l3.getNom()+"  "+l3.getPrenom());
+		//for(Patient l3:L3)
+			//	System.out.println(l3.getId()+"  "+l3.getNom()+"  "+l3.getPrenom());
 		
 		
 		// TODO : créer un nouveau patient (valeurs au choix)  PUIS enregistrer-le
-		System.out.println("----------------------------créer un nouveau patient (valeurs au choix)  PUIS enregistrer-le-----------------------------------");
+		//System.out.println("----------------------------créer un nouveau patient (valeurs au choix)  PUIS enregistrer-le-----------------------------------");
 		
 		//Date d3 = new Date(119,1,22);
 		
@@ -78,23 +81,23 @@ public class Sb002Application implements CommandLineRunner {
 	        
 	    //    Calendar c = Calendar.getInstance();
 	    //    System.out.println(c);
-		Date d1 = new Date(54,10,1); 
+		//Date d1 = new Date(54,10,1); 
 		
 	         
-	    patientRepository.save(new Patient(11,"ALi","Ammar","M" , d1,"122333","Bab Ezzouar"));
-		System.out.println("Patient ALi Ammar est Ajouté");
+	    //patientRepository.save(new Patient(11,"ALi","Ammar","M" , d1,"122333","Bab Ezzouar"));
+		//System.out.println("Patient ALi Ammar est Ajouté");
 		
-		List<Patient> L7=patientRepository.findAll();
-		for(Patient l2:L7)
-		System.out.println(l2.getNom()+"  "+l2.getPrenom());
+		//List<Patient> L7=patientRepository.findAll();
+		//for(Patient l2:L7)
+		//System.out.println(l2.getNom()+"  "+l2.getPrenom());
 		
 		// TODO : rechercher la consultation ayant id=3 
 		
 		
-		System.out.println("--------------------------rechercher la consultation ayant id=3 -------------------------------------");
+		//System.out.println("--------------------------rechercher la consultation ayant id=3 -------------------------------------");
 		
-		Optional<Patient> L4 = patientRepository.findById(3);
-		System.out.println(L4);
+		//Optional<Patient> L4 = patientRepository.findById(3);
+		//System.out.println(L4);
 		
 			               	
 	   }
